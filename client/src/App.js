07a8517a7d324366137738ffa0c53dc226011368
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-09-22 11:37:15 -0500

    react setup fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,4 +17,3 @@ function App() {
 }
 
 export default App;
-
